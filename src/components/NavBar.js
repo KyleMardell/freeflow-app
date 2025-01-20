10a360d0d6610ee3752358eaa311643aec6eabdc
Kyle Mardell <kyleslufc@gmail.com>
2025-01-20 23:22:04 +0000

    Add navbar links and install react bootstrap.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,52 +1,41 @@
-import styles from '../styles/NavBar.module.css';
 import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className={`navbar navbar-expand-lg ${styles.NavBar}`}>
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    Free Flow
-                </a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a
-                                class="nav-link active"
-                                aria-current="page"
-                                href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Custom Tasks
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Profile
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Archive Projects
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar className={styles.NavBar} expand="lg" fixed="top">
+            <Container>
+                <NavLink to="/">
+                    <Navbar.Brand>Free Flow</Navbar.Brand>
+                </NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <NavLink
+                            exact
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/">
+                            Home
+                        </NavLink>
+                        <NavLink
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/signin">
+                            Sign in
+                        </NavLink>
+                        <NavLink
+                            to="/signup"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}>
+                            Sign up
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
