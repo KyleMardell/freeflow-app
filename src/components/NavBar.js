07ecfed1127fd451c8f9dd47b9e88bed000a972c
Kyle Mardell <kyleslufc@gmail.com>
2025-01-21 20:54:53 +0000

    Add basic styles, fonts and initial colours from the chosen colour palette.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,27 +8,27 @@ const NavBar = () => {
         <Navbar className={styles.NavBar} expand="lg" fixed="top">
             <Container>
                 <NavLink to="/">
-                    <Navbar.Brand>Free Flow</Navbar.Brand>
+                    <Navbar.Brand><h1>Free Flow</h1></Navbar.Brand>
                 </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink
                             exact
-                            className={styles.NavLink}
+                            className={`${styles.NavLink} text-center`}
                             activeClassName={styles.Active}
                             to="/">
                             Home
                         </NavLink>
                         <NavLink
-                            className={styles.NavLink}
+                            className={`${styles.NavLink} text-center`}
                             activeClassName={styles.Active}
                             to="/signin">
                             Sign in
                         </NavLink>
                         <NavLink
                             to="/signup"
-                            className={styles.NavLink}
+                            className={`${styles.NavLink} text-center`}
                             activeClassName={styles.Active}>
                             Sign up
                         </NavLink>
