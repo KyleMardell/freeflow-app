651d32c07161a44077c65810264e997134684584
Kyle Mardell <kyleslufc@gmail.com>
2025-01-27 22:50:15 +0000

    Add layout styling to profile page.

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
-import { Col, Row } from "react-bootstrap";
+
+import { Button, Col, Row } from "react-bootstrap";
 import UserImage from "../../components/UserImage";
+
+import buttonStyles from "../../styles/Button.module.css";
+import styles from "../../styles/ProfilePage.module.css";
 
 const ProfilePage = ({ profile_id }) => {
     const [profile, setProfile] = useState({});
@@ -23,43 +27,52 @@ const ProfilePage = ({ profile_id }) => {
 
     return (
         <>
-          <Row>
-              <Col>
-                <h1>Profile</h1>
-              </Col>
-            </Row>
-            <Row>
-                <Col>
-                <UserImage src={image} />
-                    <div>
-                        <p>Username: {owner}</p>
-                        <p>Name: {name}</p>
-                    </div>
-                    
+            <Row className="d-flex justify-content-center">
+                <Col className="text-center my-4" lg={8}>
+                    <h1>Profile</h1>
                 </Col>
             </Row>
-            <Row>
-              <Col>
-                <p>Bio: {bio}</p>
-              </Col>
+            <Row className="px-1 d-flex justify-content-center">
+                <Col className={`${styles.DetailBorder} d-flex p-1`} lg={8}>
+                    <div className="d-flex align-items-center justify-content-center">
+                      <UserImage src={image} />
+                    </div>
+                    <div>
+                        <p className="p-1 m-0">Username: {owner}</p>
+                        <p className="p-1 m-0">Name: {name}</p>
+                    </div>
+                </Col>
             </Row>
-            <Row>
-              <Col>
-                <p>Email: {email}</p>
-              </Col>
+            <Row className="px-1 d-flex justify-content-center">
+                <Col className={`${styles.DetailBorder} p-1`} lg={8}>
+                    <p className="p-1 m-0">Bio: {bio}</p>
+                </Col>
             </Row>
-            <Row>
-              <Col>
-                <p>Phone: {phone}</p>
-              </Col>
+            <Row className="px-1 d-flex justify-content-center">
+                <Col className={`${styles.DetailBorder} p-1`} lg={8}>
+                    <p className="p-1 m-0">Email: {email}</p>
+                </Col>
             </Row>
-            <Row>
-              <Col>
-                <p>Joined: {created_at}</p>
-              </Col>
-              <Col>
-                <p>Last updated: {updated_at}</p>
-              </Col>
+            <Row className="px-1 d-flex justify-content-center">
+                <Col className={`${styles.DetailBorder} p-1`} lg={8}>
+                    <p className="p-1 m-0">Phone: {phone}</p>
+                </Col>
+            </Row>
+            <Row className="px-1 d-flex justify-content-center">
+                <Col className={`${styles.DetailBorder} p-1`} xs={6} lg={4}>
+                    <p className="p-1 m-0">Joined: {created_at}</p>
+                </Col>
+                <Col className={`${styles.DetailBorder} p-1`}xs={6} lg={4}>
+                    <p className="p-1 m-0">Updated: {updated_at}</p>
+                </Col>
+            </Row>
+            <Row className="px-1 my-4 d-flex justify-content-center">
+                <Col className="p-0 m-0" lg={8}>
+                    <Button
+                        className={`${buttonStyles.Button} ${buttonStyles.Wide}`}>
+                        Edit Profile
+                    </Button>
+                </Col>
             </Row>
         </>
     );
