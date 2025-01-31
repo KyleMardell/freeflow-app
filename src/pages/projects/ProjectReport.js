b2756aa2ceda47d73c5ffb308352bca34bee1fb1
Kyle Mardell <kyleslufc@gmail.com>
2025-01-31 00:21:40 +0000

    Add task reports to the project report page.

--- src/pages/projects/ProjectReport.js
+++ src/pages/projects/ProjectReport.js
@@ -12,6 +12,7 @@ const ProjectReport = ({ profile_id }) => {
     const [project, setProject] = useState({});
     const { title, brief, hourly_rate } = project;
     const [tasks, setTasks] = useState([]);
+    const [taskReports, setTaskReports] = useState([]);
 
     const [actualTotalHours, setActualTotalHours] = useState(0.0);
     const [estimatedTotalHours, setEstimatedTotalHours] = useState(0.0);
@@ -47,26 +48,31 @@ const ProjectReport = ({ profile_id }) => {
             let estimatedTotalHrs = 0.0;
             let estimatedCost = 0.0;
             let actualCost = 0.0;
-
             const rate = parseFloat(hourly_rate) || 0;
 
-            for (const task of tasks) {
-                const actualHours = parseFloat(task.actual_time);
-                const estimatedHours = parseFloat(task.estimated_time);
+            const newTaskReports = tasks.map((task) => {
+                const actualHours = parseFloat(task.actual_time) || 0;
+                const estimatedHours = parseFloat(task.estimated_time) || 0;
 
-                if (actualHours > 0) {
-                    actualTotalHrs += actualHours;
-                }
-                if (estimatedHours > 0) {
-                    estimatedTotalHrs += estimatedHours;
-                }
-            }
+                actualTotalHrs += actualHours;
+                estimatedTotalHrs += estimatedHours;
+
+                return {
+                    id: task.id,
+                    title: task.title,
+                    actual_time: actualHours,
+                    estimated_time: estimatedHours,
+                    actual_cost: actualHours * rate,
+                    estimated_cost: estimatedHours * rate,
+                };
+            });
 
             if (rate > 0) {
                 actualCost = rate * actualTotalHrs;
                 estimatedCost = rate * estimatedTotalHrs;
             }
 
+            setTaskReports(newTaskReports);
             setActualTotalHours(actualTotalHrs);
             setEstimatedTotalHours(estimatedTotalHrs);
             setTotalActualCost(actualCost);
@@ -96,26 +102,62 @@ const ProjectReport = ({ profile_id }) => {
                 <Col className="px-1" xs={12} lg={8}>
                     <Row>
                         <Col>
-                            <p>Estimated time: {estimatedTotalHours} hours</p>
+                            <p className="m-0 mb-1">Estimated time: {estimatedTotalHours} hours</p>
                         </Col>
                         <Col>
-                            <p>Actual time: {actualTotalHours} hours</p>
+                            <p className="m-0 mb-1">Actual time: {actualTotalHours} hours</p>
                         </Col>
                     </Row>
                     <Row>
                         <Col>
-                            <p>Estimated cost: £{totalEstimatedCost}</p>
+                            <p className="m-0 mb-1">Estimated cost: £{totalEstimatedCost}</p>
                         </Col>
                         <Col>
-                            <p>Actual cost: £{totalActualCost}</p>
+                            <p className="m-0 mb-1">Actual cost: £{totalActualCost}</p>
                         </Col>
                     </Row>
                 </Col>
             </Row>
 
-            <Row className="d-flex justify-content-center">
+            <Row className="d-flex justify-content-center p-1">
+                <Col className="px-1 py-4" xs={12} lg={8}>
+                    <h3>Task Reports</h3>
+                </Col>
+
+                {taskReports ? (
+                    taskReports.map((report) => {
+                        return (
+                            <Col className="p-1" xs={12} lg={8} key={report.id}>
+                                <h4 className="m-0">{report.title}</h4>
+                                <Row>
+                                    <Col>
+                                        <p className="m-0 mb-1">Est time: {report.estimated_time} hrs</p>
+                                    </Col>
+                                    <Col>
+                                        <p className="m-0 mb-1">Est cost: £{report.estimated_cost}</p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <p className="m-0 mb-1">Total time: {report.actual_time}</p>
+                                    </Col>
+                                    <Col>
+                                        <p className="m-0 mb-1">Total cost: ${report.actual_cost}</p>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        );
+                    })
+                ) : (
+                    <Col>
+                        <p>No task reports </p>
+                    </Col>
+                )}
+            </Row>
+
+            <Row className="d-flex justify-content-center my-2">
                 <Col
-                    className="px-1 d-flex justify-content-center align-items-center"
+                    className="p-1 d-flex justify-content-center align-items-center"
                     xs={3}
                     lg={2}>
                     <UserImage src={image} />
