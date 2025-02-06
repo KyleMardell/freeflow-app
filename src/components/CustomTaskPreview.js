8aacead688cb0a62e320e07db08b4cfbb5397f86
Kyle Mardell <kyleslufc@gmail.com>
2025-02-06 23:06:08 +0000

    Adjust custom task preview styles.

--- src/components/CustomTaskPreview.js
+++ src/components/CustomTaskPreview.js
@@ -18,9 +18,9 @@ const CustomTaskPreview = (props) => {
             <div className={`${styles.Preview} my-2 mx-0`}>
                 
                 <div className="text-center">
-                    <h2>{title}</h2>
+                    <h2 className="mb-0 mt-1">{title}</h2>
                 </div>
-                <div className="d-flex justify-content-between">
+                <div className="d-flex justify-content-between p-1">
                     <span>Estimated: {estimated_time}</span>
                     <span>Average: {average_time}</span>
                     <span>Uses: {frequency}</span>
